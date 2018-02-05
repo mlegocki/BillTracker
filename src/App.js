d1fc1c4f1c4ae7d7eecaa6fc66053c7095ac2e7c
Maxwell Legocki <mdlegocki@gmail.com>
2018-02-05 09:00:27 -0500

    resolved chrome storage issue

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* global chrome */
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react'
 import AddBill from './AddBill';
@@ -16,6 +17,13 @@ class App extends Component {
   toggleDisplay() {
     this.state.displayPortal ? this.setState({ displayPortal: false }) : this.setState({ displayPortal: true });
   }
+
+  // save() {
+  //   chrome.storage.sync.set({ "yourBody": "myBody" }, function () {
+  //     //  A data saved callback omg so fancy
+  //   });
+  // }
+
   render() {
     return (
       <div className="App">
